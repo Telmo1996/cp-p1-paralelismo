b72a1f1912eab7f7e4ebd4408eefd6ec02b9b7c5
Telmo1996 <tfercor@gmail.com>
2021-04-13 20:15:32 +0200

    done

--- p1.c
+++ p1.c
@@ -2,12 +2,12 @@
 #include <math.h>
 #include <mpi.h>
 
-double calculaPi(int numProc, int n){
+double calculaPi(int numProc, int numProcs, int n){
 	double h, sum, x;
 
 	h   = 1.0 / (double) n;
 	sum = 0.0;
-	for (int i = 1; i <= n; i++) {
+	for (int i = numProc; i <= n; i += numProcs) {
 		x = h * ((double)i - 0.5);
 		sum += 4.0 / (1.0 + x*x);
 	}
@@ -23,7 +23,8 @@ int main(int argc, char *argv[])
     double PI25DT = 3.141592653589793238462643;
     double pi, h, sum, x;
 	int numProcs, rank;
-	int N[1], Pi[1];
+	int N[1];
+	double Pi[1];
 
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -36,23 +37,33 @@ int main(int argc, char *argv[])
 			printf("Enter the number of intervals: (0 quits) \n");
 			scanf("%d",&n);
 
-			if (n == 0) break;
-			
 			N[0] = n;
-			for(i=1; i<numProcs; i++)
+			for(i=1; i<numProcs; i++){
 				MPI_Send(N, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+			}
 
-			//pi = calculaPi(0, n);
+			if (n == 0) break;
 
-			//printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+			pi = calculaPi(0, numProcs, n);
+
+			for(i=1; i<numProcs; i++){
+				MPI_Recv(Pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				pi += Pi[0];
+			}
+
+			printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
 		}
 
 	}else{
-		MPI_Recv(N, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//printf("%d", N[0]);
+		while(!done){
+			MPI_Recv(N, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			if (N[0] == 0) break;
 
-		pi = calculaPi(rank, N[0]);
-		
+			pi = calculaPi(rank, numProcs, N[0]);
+			
+			Pi[0] = pi;
+			MPI_Send(Pi, 1, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD);
+		}
 
 	}
 
