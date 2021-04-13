22d1674f4119001ed25060260e2d857cfe74e7c7
Telmo Fernández Corujo <telmo.fcorujo@udc.es>
2021-04-13 13:47:54 +0200

    calculo separado en funcion

--- p1.c
+++ p1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <math.h>
-#include <mpi.h>
+//#include <mpi.h>
+
+double calculaPi(int numProc, int n){
+	double h, sum, x;
+
+	h   = 1.0 / (double) n;
+	sum = 0.0;
+	for (int i = 1; i <= n; i++) {
+		x = h * ((double)i - 0.5);
+		sum += 4.0 / (1.0 + x*x);
+	}
+	return h * sum;
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,13 +28,7 @@ int main(int argc, char *argv[])
     
         if (n == 0) break;
   
-        h   = 1.0 / (double) n;
-        sum = 0.0;
-        for (i = 1; i <= n; i++) {
-            x = h * ((double)i - 0.5);
-            sum += 4.0 / (1.0 + x*x);
-        }
-        pi = h * sum;
+		pi = calculaPi(0, n);
 
         printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
     }
