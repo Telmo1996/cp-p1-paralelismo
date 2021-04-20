d61851a820645f3ac69d91f2347d3457cb984b6b
Telmo Fernández Corujo <telmo.fcorujo@udc.es>
2021-04-20 15:24:24 +0200

    eliminado array

--- p1.c
+++ p1.c
@@ -23,8 +23,7 @@ int main(int argc, char *argv[])
     double PI25DT = 3.141592653589793238462643;
     double pi, h, sum, x;
 	int numProcs, rank;
-	int N[1];
-	double Pi[1];
+	double pi_parcial;
 
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -37,9 +36,8 @@ int main(int argc, char *argv[])
 			printf("Enter the number of intervals: (0 quits) \n");
 			scanf("%d",&n);
 
-			N[0] = n;
 			for(i=1; i<numProcs; i++){
-				MPI_Send(N, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+				MPI_Send(&n, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
 			}
 
 			if (n == 0) break;
@@ -47,8 +45,8 @@ int main(int argc, char *argv[])
 			pi = calculaPi(0, numProcs, n);
 
 			for(i=1; i<numProcs; i++){
-				MPI_Recv(Pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				pi += Pi[0];
+				MPI_Recv(&pi_parcial, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				pi += pi_parcial;
 			}
 
 			printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
@@ -56,13 +54,12 @@ int main(int argc, char *argv[])
 
 	}else{
 		while(!done){
-			MPI_Recv(N, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			if (N[0] == 0) break;
+			MPI_Recv(&n, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			if (n == 0) break;
 
-			pi = calculaPi(rank, numProcs, N[0]);
+			pi = calculaPi(rank, numProcs, n);
 			
-			Pi[0] = pi;
-			MPI_Send(Pi, 1, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD);
+			MPI_Send(&pi, 1, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD);
 		}
 
 	}
